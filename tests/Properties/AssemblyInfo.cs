a29de33326b8b14b636831f9cfd2b7d7f695c61a
dennisvg111 <dennisvg111@gmail.com>
2024-09-20 19:25:48 +0200

    Initial commit

---
+++ tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f2aad37-dd33-4c84-9ca4-92a12d829468")]
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace DG.Heuristic.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
