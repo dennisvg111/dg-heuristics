20e85b5bfb0a464d5d5c3e603cd57fa61eb112a7
dennisvg111 <dennisvg111@gmail.com>
2025-03-21 19:20:38 +0100

    Add test for knapsack numbers that are larger than the max

---
+++ tests/Collections/SubsetSumKnapsackTests.cs
@@ -0,0 +1,20 @@
+﻿using DG.Heuristic.Collections;
+using FluentAssertions;
+using Xunit;
+
+namespace DG.Heuristic.Tests.Collections
+{
+    public class SubsetSumKnapsackTests
+    {
+        [Fact]
+        public void PickClosest_NumbersAboveMax_CanBeFound()
+        {
+            var knapsack = new SubsetSumKnapsack(21);
+            knapsack.Add(3000);
+
+            knapsack.PickClosest(out int sum);
+
+            sum.Should().Be(3000);
+        }
+    }
+}
