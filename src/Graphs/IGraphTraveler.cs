96e89735fb4415d6f603041817f1c9f732c47605
Dennis van Gils <dennisvg111@gmail.com>
2025-04-14 10:00:43 +0200

    Add generic traveling salesman interface

---
+++ src/Graphs/IGraphTraveler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DG.Heuristic.Graphs
+{
+    public interface IGraphTraveler<TData> where TData : IPositionedData
+    {
+        void Add(TData data);
+
+        List<TData> CalculateRoute(out double totalDistance);
+    }
+}
